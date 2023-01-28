8d289a58a227f37b29a544df08343e111119a729
dharmateja <dharmateja.176@gmail.com>
2023-01-28 20:47:03 +0530

    Second Commit

--- src/Expenses.java
+++ src/Expenses.java
@@ -10,8 +10,8 @@ class Expenses{
 		double total = branding + travel + food + logistics;
 		System.out.printf("Total expenses : Rs.%.2fn", total);
 		System.out.printf("Branding expenses percentage : %.2f%%\n", (branding/total)*100);
-		System.out.printf("Travel expenses percentage : %.299\n", (travel/total) *100);
-		System.out.printf("Food expenses percentage : %.2f%9n", (food/total) *100);
-		System.out.printf("Logistics expenses percentage : %.2f%%n", (logistics/total) *100);
+		System.out.printf("Travel expenses percentage : %.2f%%\n", (travel/total) *100);
+		System.out.printf("Food expenses percentage : %.2f%%\n", (food/total) *100);
+		System.out.printf("Logistics expenses percentage : %.2f%%\n", (logistics/total) *100);
 	}
 }
